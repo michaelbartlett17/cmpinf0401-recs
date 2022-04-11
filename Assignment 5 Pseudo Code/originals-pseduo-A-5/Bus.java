71395d6bbde83c712bc03406b7c68c53bd657365
michael <mbmichaelman@icloud.com>
2022-04-11 18:01:50 -0400

    slide updates

--- Assignment 5 Pseudo Code/originals-pseduo-A-5/Bus.java
+++
@@ -1,7 +0,0 @@
-public class Bus extends Vehicle { // Make sure to extend Vehicle
-    // Add datafield for number of passengers that the Bus can hold
-
-    // Add setters and getters for this field
-
-    // Modify the stats method to reflect this field.
-}
