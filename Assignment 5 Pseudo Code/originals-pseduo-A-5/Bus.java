66ba3153ae8b51b0f15a7196d0c6c967e60f5ad1
michael <mbmichaelman@icloud.com>
2022-04-11 18:00:45 -0400

    slide updates

---
+++ Assignment 5 Pseudo Code/originals-pseduo-A-5/Bus.java
@@ -0,0 +1,7 @@
+public class Bus extends Vehicle { // Make sure to extend Vehicle
+    // Add datafield for number of passengers that the Bus can hold
+
+    // Add setters and getters for this field
+
+    // Modify the stats method to reflect this field.
+}
---
+++ Assignment 5 Pseudo Code/originals-pseduo-A-5/CT.java
@@ -0,0 +1,7 @@
+public class CT extends Truck { // Make sure to extend Truck, since it extends Truck, make sure to make Truck's datafields make Truck's datafields protected.
+    // Add datafield of type boolean named isCombination to indicate if this is a combinational vehicle
+
+    // Add setter and getters for the datafield
+
+    // Modify the stats method to reflect the new field
+}
